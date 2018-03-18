55fa52e34d8a9abf59912076a468428e5d952e14
mac <mac@macs-MacBook-Pro.local>
2018-03-18 00:44:37 -0600

    almost done

--- gif.js
+++ gif.js
@@ -1,19 +1,37 @@
 $(document).ready(function(){
 var topics = [$("#arrival"), $("#close"), $("#ex"), $("#solaris"), $("#2001")];
-var b = $("<button>");
-//var c = topics[topics.length - 1];
 
-$("#search").on("click", kendrick);
-$(".film").on("click", joni);
-$(".frames").on("click", paula);
+$("#search").on("click", fi);
 
-function paula() {
-    $("body").css("background-color", "red");
+$(".film").on("click", sci);
+
+
+
+function fi() {
+
+if ($("#movie").val() == "") {
+        alert("no");
+    }
+
+    else {
+        var b = $("<button>");
+        b.text($("#movie").val());
+        $("#buttons").append(b);
+        topics.push(b);
+        b.attr("id", $("#movie").val());
+        b.attr("data-movie", $("#movie").val());
+        
+       
+           
+            b.attr("class", "film");
+            $(".film").on("click", sci);
+
+            }
 }
 
-function joni() {
-    
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+function sci() {
+    $("#cinema").empty();
+   var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
     $(this).attr("data-movie") + "&api_key=wx9p7JJJbTuqM9W2mB3pMgK9Jo0cJiGN&limit=10";
     $.ajax({
         url: queryURL,
@@ -21,59 +39,35 @@ function joni() {
       })
       .then(function(response) {
         var results = response.data;
+
         for (var i = 0; i < results.length; i++) {
+            console.log(results);
+
           var gifDiv = $("<div class='frames'>");
+          
           var rating = results[i].rating;
           var p = $("<p>").text("Rating: " + rating);
-          var personImage = $("<img>");
-          personImage.attr("src", results[i].images.fixed_height_still.url);
+          var movieImage = $("<img class='still'>");
+          var stanley = results[i].images.fixed_height_still.url;
+          var kubrick = results[i].images.fixed_height.url;
+         
+          movieImage.attr("src", stanley);
           gifDiv.prepend(p);
-          gifDiv.prepend(personImage);
-          $("#gifs-appear-here").prepend(gifDiv);
-          $(".frames").on("click", paula);
-    
-        }
-      });
+          gifDiv.prepend(movieImage);
         
-    }
-   
+          $("#cinema").prepend(gifDiv);
+         
+          
+          $(".still").on("click", function() {
+              var blade = $(this).attr("src");
+              $(this).attr("src", kubrick);
+              console.log(blade);
+          });
 
-
-
-
-
-
-function kendrick() {
-
-
-    
-
-    if ($("#movie").val() == "") {
-        alert("no");
-    }
-
-    else {
-       
-
-        $("#buttons").empty();
-        topics.push(b);
-        b.attr("id", $("#movie").val());
-        b.attr("data-movie", $("#movie").val());
-
-       
-        setTimeout(function() { $("#buttons").append(topics); }, 0010);
-       
-            b.text($("#movie").val());
-           $("#buttons").append(b);
-            b.attr("class", "film");
-            
-            $(".film").on("click", joni);
-            
-            
-
-}
-
-}
-
+         
+          
+         }
+    });  
+      }
 });
 
